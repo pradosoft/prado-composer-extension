05fa51cdbac25aab8eeef9b7db50e3d4186ab55b
Belisoful <belisoful@icloud.com>
2026-03-27 04:24:54 +0000

    php-cs-fixer configuration, from PRADO.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
 $finder = PhpCsFixer\Finder::create()
+	->exclude('.git/')
 	->exclude('docs/')
 	->exclude('tests/')
 	->exclude('vendor/')
@@ -12,31 +18,34 @@ $config
     ->setIndent("\t")
     ->setLineEnding("\n")
 	->setRules([
-		'@PSR2' => true,
+		'@PSR12' => true,
+		'@PHP82Migration' => true,
 		'align_multiline_comment' => true,
-		'array_syntax' => ['syntax' => 'short'],
 		'binary_operator_spaces' => true,
 		'blank_line_after_namespace' => true,
 		'blank_line_after_opening_tag' => true,
 		'cast_spaces' => ['space' => 'single'],
+		'clean_namespace' => true,
 		'combine_nested_dirname' => true,
 		'concat_space' => ['spacing' => 'one'],
 		'dir_constant' => true,
 		'is_null' => true,
-		'list_syntax' => ['syntax' => 'short'],
+		'type_declaration_spaces' => ['elements' => ['function', 'property']],
+		'method_chaining_indentation' => true,
 		'modernize_types_casting' => true,
 		'no_alias_functions' => true,
+		'no_blank_lines_after_phpdoc' => true,
 		'no_null_property_initialization' => true,
-		'no_whitespace_before_comma_in_array' => true,
 		'phpdoc_add_missing_param_annotation' => ['only_untyped' => false],
+		'phpdoc_indent' => true,
+		'phpdoc_no_package' => true,
 		'phpdoc_order' => true,
 		'phpdoc_scalar' => true,
+		'phpdoc_types' => true,
 		'phpdoc_types_order' => true,
 		'psr_autoloading' => true,
 		'ternary_operator_spaces' => true,
-		'ternary_to_null_coalescing' => true,
 		'trim_array_spaces' => true,
-		'visibility_required' => true,
 		'whitespace_after_comma_in_array' => true,
 	])
 	->setFinder($finder);
